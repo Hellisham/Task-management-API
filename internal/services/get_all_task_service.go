341f5cb6d7214b67814f92122828ebfb638df788
hellisham <ashkan.m.m.t@gmail.com>
2024-12-24 12:14:04 +0330

    adding service and handler

---
+++ internal/services/get_all_task_service.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	task "github.com/Hellisham/task_manager/internal/models"
+	"gorm.io/gorm"
+)
+
+type TaskService struct {
+	DB *gorm.DB
+}
+
+func NewTaskService(db *gorm.DB) *TaskService {
+	return &TaskService{
+		DB: db,
+	}
+}
+
+func (service *TaskService) GetAllTasks() ([]task.Task, error) {
+	var tasks []task.Task
+	service.DB.Find(&tasks)
+	if service.DB.Error != nil {
+		return nil, service.DB.Error
+	}
+	return tasks, nil
+}
